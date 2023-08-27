3a4597b838de9c4c671ef0618312a038f838fbc3
MaryamHam <107949861+MaryamHam@users.noreply.github.com>
2023-08-27 19:30:19 +0400

    Future Example

---
+++ bin/future.dart
@@ -0,0 +1,46 @@
+
+void main() {
+ 
+Future.delayed(Duration(seconds: 1),(){
+  showFi();
+});
+
+Future.delayed(Duration(seconds: 3),(){
+  showFou();
+});
+Future.delayed(Duration(seconds: 4),(){
+  showThi();
+});
+Future.delayed(Duration(seconds: 6),(){
+  showSec();
+});
+Future.delayed(Duration(seconds: 7),(){
+  show();
+})
+
+.then((value){
+  print("Finished");
+  
+} );
+
+
+
+}
+
+show(){
+  print("1");
+}
+
+showSec(){
+  print("2");
+}
+showThi(){
+  print("3");
+}
+showFou(){
+  print("4");
+}
+showFi(){
+  print("5");
+}
+
